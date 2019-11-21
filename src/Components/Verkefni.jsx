df83a719108c4c2981ba1686e16612127a1fe043
mani16 <mani16@ru.is>
2019-11-21 16:01:24 +0000

    basic setup a verkefnum

--- src/Components/Verkefni.jsx
+++ src/Components/Verkefni.jsx
@@ -1,67 +1,115 @@
 import React, { Component } from 'react';
+import hhusafell from "../Assets/projects/hotelhusaimg.png";
+import hreykholt from "../Assets/projects/hotelreykholtimg.jpg";
+import husafellbistro from "../Assets/projects/husafellbistroimg.jpg";
+import krauma from "../Assets/projects/kraumaimg.jpg";
+import grunnborg from "../Assets/projects/grunnborgimg.jpg";
+import foodstation from "../Assets/projects/foodstationimg.jpg";
+import leikskoli from "../Assets/projects/leikskoliimg.png";
+import einbylishus from "../Assets/projects/einbylishusimg.jpg";
+import sumarbustadir from "../Assets/projects/sumarhusimg.jpg";
+import arnarstapi from "../Assets/projects/arnarstapiimg.jpg";
+
 class Verkefni extends Component {
     render() {
         return (
             <div>
                 <h1>Verkefni</h1>
-                <div className="verkcontainer">
-                    <h2>
-                        Hótel Húsafell
-                    </h2>
+                <div className="verkefnaContainer">
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={hhusafell} />
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={hreykholt} />
+  
+                </div>
+                <div className="verkefnatextaContainer">
+                    <a>
+                        <h2>
+                            Hótel Húsafell
+                        </h2>
+
+                        <h2>
+                            Hótel Reykholt
+                        </h2>
+                    </a>
+                </div>
+                <div className="verkefnaContainer">
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={husafellbistro} />
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={krauma} />
+
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Bistro Húsafell
-                    </h2>
+                <div className="verkefnatextaContainer2">
+                    <a>
+                        <h2>
+                            Húsafell Bistro
+                        </h2>                    
+                        <h2>
+                            Krauma
+                        </h2>
+                    </a>
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Hótel Reykholt
-                    </h2>
+                <div className="verkefnaContainer">
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={grunnborg} />
+                
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={foodstation} />
+
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Krauma
-                    </h2>
+                <div className="verkefnatextaContainer3">
+                    <a>
+                        <h2>
+                            Grunnskólinn Borgarnesi
+                        </h2>
+
+                        <h2>
+                            Digranesgata 4
+                        </h2>
+                    </a>
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Grunnskólinn Borgarnesi
-                    </h2>
+                <div className="verkefnaContainer">
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={leikskoli} />
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={einbylishus} />
+
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Digranesgata 4
-                    </h2>
+                <div className="verkefnatextaContainer4">
+                    <a>
+                        <h2>
+                            Leikslólinn við GBF
+                        </h2>
+
+                        <h2>
+                            Einbýlishús
+                        </h2>
+                    </a>
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Leikslólinn við GBF
-                    </h2>
+                <div className="verkefnaContainer">
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={sumarbustadir} />
+
+                <img alt="Rafkaup" className="verkefnimynd fade-in" src={arnarstapi} />
+
                 </div>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Einbýlishús
-                    </h2>
-                </div>
+                <div className="verkefnatextaContainer5">
+                    <a>
+                        <h2>
+                            Sumarbústaðir
+                        </h2>
 
-                <div className="verkcontainer">
-                    <h2>
-                        Sumarbústaðir
-                    </h2>
-                </div>
-
-                <div className="verkcontainer">
-                    <h2>
-                        Veitingastaður á Arnastapa
-                    </h2>
+                        <h2>
+                            Veitingastaður á Arnastapa
+                        </h2>   
+                    </a>
                 </div>
 
             </div>
