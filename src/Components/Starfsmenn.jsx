bf4d984e000c54a61d04ea6d270216a2a7761a41
eybbus <eythorfreyr94@gmail.com>
2019-11-25 14:38:47 +0000

    Added content file, starfsmenn loads data from said file

--- src/Components/Starfsmenn.jsx
+++ src/Components/Starfsmenn.jsx
@@ -1,100 +1,16 @@
 import React, { Component } from 'react';
 import Card from "./Card/Card";
 import "../App.css"
-import arnarholm from "../Assets/employees/arnarholm.jpg";
-import antonfreyr from "../Assets/employees/antonfreyr.jpg";
+import { employees } from "../Assets/content.js";
 
 class Starfsmenn extends Component {
-    constructor(props) {
-        super(props);
-        // Don't call this.setState() here!
-        this.state = { employees: [
-            {
-                name: "Arnar Hólmarsson",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: arnarholm,
-            },
-            {
-                name: "Gísli Már",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Hannes Þór",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Þorgeir",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Bergþór Ægir",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Jón á Báreksstöðum",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Jónas",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Anton Freyr",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: antonfreyr,
-            },
-            {
-                name: "Hjörvar Óli",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Sigurður Óskar",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-            {
-                name: "Áslaug Ella",
-                title: "Starfsmaður",
-                phone: "432-1235",
-                email: "example@example.com",
-                image: "http://via.placeholder.com/500",
-            },
-        ] };
-      }
-
     render() {
         let duration = 0.2;
-        const cards = this.state.employees.map(emp => { 
+        const cards = employees.map(emp => { 
             duration += 0.3;
             return (
-                <Card 
+                <Card
+                    key={emp.name} 
                     name={emp.name}
                     title={emp.title}
                     phone={emp.phone}
