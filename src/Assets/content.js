b6cd02f38240930aabb47a283030da032604d1ad
mani16 <mani16@ru.is>
2019-11-25 17:16:06 +0000

    projects component

--- src/Assets/content.js
+++ src/Assets/content.js
@@ -78,4 +78,57 @@ module.exports = {
             "image": "http://via.placeholder.com/500"
         }
     ],
+    "Projects": [
+        {
+            "name": "Hótel Húsafell",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/hotelhusaimg.png")
+        },
+        {
+            "name": "Hótel Reykholt",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/hotelreykholtimg.jpg")
+        },
+        {
+            "name": "Húsafell Bistro",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/husafellbistroimg.jpg")
+        },
+        {
+            "name": "Krauma",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/kraumaimg.jpg")
+        },
+        {
+            "name": "Grunnskólinn Borgarnesi",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/grunnborgimg.jpg")
+        },
+        {
+            "name": "Digranesgata 4",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/foodstationimg.jpg")
+        },
+        {
+            "name": "Leikskólinn við GBF",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/leikskoliimg.png")
+        },
+        {
+            "name": "Einbýlishús",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/einbylishusimg.jpg")
+        },
+        {
+            "name": "Sumarbústaðir",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/sumarhusimg.jpg")
+        },
+        {
+            "name": "Veitingastaður á Arnastapa",
+            "text": "SETJA TEXTA HÉRNA UM ÞETTA VERKEFNI", 
+            "image": require("./projects/arnarstapiimg.jpg")
+        }
+
+    ],
 }
