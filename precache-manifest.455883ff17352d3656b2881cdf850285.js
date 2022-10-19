9c1e9b8005cd7460b638e8878f0d76ba9790a53f
Anton <antona14@ru.is>
2022-10-19 22:26:42 +0200

    Updates

---
+++ precache-manifest.455883ff17352d3656b2881cdf850285.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee633b9b62e76504fc56a2178fa657de",
+    "url": "/antoniofreyr.github.io.git/index.html"
+  },
+  {
+    "revision": "7ae8a2013b3b8648b8c2",
+    "url": "/antoniofreyr.github.io.git/static/css/main.69f760a4.chunk.css"
+  },
+  {
+    "revision": "bcd446d5a336ccca95ee",
+    "url": "/antoniofreyr.github.io.git/static/js/2.f4241b2d.chunk.js"
+  },
+  {
+    "revision": "7ae8a2013b3b8648b8c2",
+    "url": "/antoniofreyr.github.io.git/static/js/main.cf76c708.chunk.js"
+  },
+  {
+    "revision": "9c814205a3acfb86dac4",
+    "url": "/antoniofreyr.github.io.git/static/js/runtime-main.44e35a8e.js"
+  },
+  {
+    "revision": "d46f1220f44f188fd80c1aa1001b28fe",
+    "url": "/antoniofreyr.github.io.git/static/media/Iceland.d46f1220.svg"
+  },
+  {
+    "revision": "33bc2a4c344be4c0709a1cdf9866884a",
+    "url": "/antoniofreyr.github.io.git/static/media/VerdskraContent.33bc2a4c.bin"
+  },
+  {
+    "revision": "4b385afcbbc7898e16b63408941fd38d",
+    "url": "/antoniofreyr.github.io.git/static/media/antonDK.4b385afc.jpg"
+  },
+  {
+    "revision": "891a7bbde0ce3c852a25c618264ab371",
+    "url": "/antoniofreyr.github.io.git/static/media/arnarholm.891a7bbd.jpg"
+  },
+  {
+    "revision": "1dd13f083ed477a62df7350b83dc78c3",
+    "url": "/antoniofreyr.github.io.git/static/media/arnarstapiimg.1dd13f08.jpg"
+  },
+  {
+    "revision": "e7fbe6268bd8809cbf0f2bca975aabc8",
+    "url": "/antoniofreyr.github.io.git/static/media/einbylishusimg.e7fbe626.jpg"
+  },
+  {
+    "revision": "ec1bde2da531a4c3e49a7fbd557f3e82",
+    "url": "/antoniofreyr.github.io.git/static/media/foodstationimg.ec1bde2d.jpg"
+  },
+  {
+    "revision": "e1d05ad817aeec5fcbb11df1eca09290",
+    "url": "/antoniofreyr.github.io.git/static/media/giljabodimg.e1d05ad8.jpg"
+  },
+  {
+    "revision": "ab4ddef2e0e679e5a361f5255a56b4e4",
+    "url": "/antoniofreyr.github.io.git/static/media/grunnborgimg.ab4ddef2.jpg"
+  },
+  {
+    "revision": "c893858a1fcfb367e76f4e9286218506",
+    "url": "/antoniofreyr.github.io.git/static/media/hannes.c893858a.jpg"
+  },
+  {
+    "revision": "9effca284ba875f623d758283bca0275",
+    "url": "/antoniofreyr.github.io.git/static/media/hotelhusaimg.9effca28.png"
+  },
+  {
+    "revision": "b92c000737eb89536e4ce41a38497961",
+    "url": "/antoniofreyr.github.io.git/static/media/hotelreykholtimg.b92c0007.jpg"
+  },
+  {
+    "revision": "ed362ec1ca7340902432886b8feb522e",
+    "url": "/antoniofreyr.github.io.git/static/media/husafellbistroimg.ed362ec1.jpg"
+  },
+  {
+    "revision": "f085a73375dae4fe44b759c48f09568a",
+    "url": "/antoniofreyr.github.io.git/static/media/husafellbustadirimg.f085a733.jpg"
+  },
+  {
+    "revision": "743d7f091ce5369a756b5bfa62e16bce",
+    "url": "/antoniofreyr.github.io.git/static/media/kraumaimg.743d7f09.jpg"
+  },
+  {
+    "revision": "de6eaf6cec35391fbea36ea71d9c1aec",
+    "url": "/antoniofreyr.github.io.git/static/media/leikskoliimg.de6eaf6c.png"
+  },
+  {
+    "revision": "b9414f8f952f9b364dc04b208b017e78",
+    "url": "/antoniofreyr.github.io.git/static/media/lumexlogo.b9414f8f.jpg"
+  },
+  {
+    "revision": "c9242a9482cceab0d872eea443a24c0b",
+    "url": "/antoniofreyr.github.io.git/static/media/rafkauplogo.c9242a94.png"
+  },
+  {
+    "revision": "f72d7e2b802d815dd26f3aec132d3748",
+    "url": "/antoniofreyr.github.io.git/static/media/sumarhusimg.f72d7e2b.jpg"
+  }
+]);
